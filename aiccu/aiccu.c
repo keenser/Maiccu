55339ba78861650de8e3dd0f6b6a24b18920a695
German Skalauhov <keen@mail15.com>
2014-01-31 16:53:04 +0400

    remove unneeded aiccu dependences

--- aiccu/aiccu.c
+++ aiccu/aiccu.c
@@ -276,6 +276,7 @@ void aiccu_install(void)
 	aiccu_os_install();
 }
 
+#if 0
 bool aiccu_setup(struct TIC_Tunnel *hTunnel, bool firstrun)
 {
 	bool ret = false;
@@ -329,6 +330,7 @@ void aiccu_beat(struct TIC_Tunnel *hTunnel)
 
 	/* L2TP Hello's are handled inside TeePee */
 }
+#endif
 
 void aiccu_reconfig(struct TIC_Tunnel *hTunnel)
 {
@@ -342,11 +344,13 @@ void aiccu_delete(struct TIC_Tunnel *hTunnel)
 	if (!g_aiccu->noconfigure) aiccu_os_delete(hTunnel);
 }
 
+#if 0
 void aiccu_test(struct TIC_Tunnel *hTunnel, bool automatic)
 {
 	D(dolog(LOG_DEBUG, "aiccu_test()\n"));
 	aiccu_os_test(hTunnel, automatic);
 }
+#endif
 
 bool aiccu_exec(const char *fmt, ...)
 {
