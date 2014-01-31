a2ec65bbb1b7c41c1cf6fbfa5111cee3c3b7266d
German Skalauhov <keen@mail15.com>
2014-01-31 16:41:28 +0400

    Add generic Adapter

---
+++ maiccu/genericAdapter.h
@@ -0,0 +1,23 @@
+//
+//  genericAdapter.h
+//  maiccu
+//
+//  Created by German Skalauhov on 30/01/2014.
+//  Copyright (c) 2014 Kristof Hannemann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface genericAdapter : NSObject
+
+- (NSInteger) loginToTicServer:(NSString *)server withUsername:(NSString *)username andPassword:(NSString *)password;
+- (void) logoutFromTicServerWithMessage:(NSString *)message;
+- (NSArray *)requestTunnelList;
+- (NSDictionary *)requestTunnelInfoForTunnel:(NSString *)tunnel;
+
+- (BOOL)saveConfig:(NSDictionary *)config toFile:(NSString *)path;
+- (NSDictionary *)loadConfigFile:(NSString *)path;
+
+- (void)startStopFrom:(NSString *)path withConfigFile:(NSString *)configPath;
+
+@end
